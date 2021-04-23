d70927510da033861ed9d831619c6a6bba24a4d0
Danilo Marques <danilomarques20@hotmail.com>
2021-04-23 14:49:52 -0300

    Implementando e utilizando o proxy de cache para evitar que o json seja sempre lido a cada chamado do metodo

--- src/main/java/br/edu/ifpb/padroes/api/damenos/proxy/DamenosServiceProxy.java
+++ src/main/java/br/edu/ifpb/padroes/api/damenos/proxy/DamenosServiceProxy.java
@@ -1,14 +1,26 @@
 package br.edu.ifpb.padroes.api.damenos.proxy;
 
 import br.edu.ifpb.padroes.api.damenos.DamenosPizza;
+import br.edu.ifpb.padroes.api.damenos.DamenosServiceImpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // TODO - implementar proxy de cache para evitar leitura do json a cada chamada do método
 public class DamenosServiceProxy implements DamenosService {
+    private DamenosServiceImpl damenosService;
+    private List<DamenosPizza> pizzasCache = new ArrayList<>();
+
+    public DamenosServiceProxy(DamenosServiceImpl damenosService) {
+        this.damenosService = damenosService;
+    }
+
     @Override
     public List<DamenosPizza> getPizzas() {
-        // TODO - implementar proxy
-        return null;
+        if (pizzasCache.isEmpty()) {
+            this.pizzasCache = damenosService.getPizzas();
+        }
+
+        return this.pizzasCache;
     }
 }
--- src/main/java/br/edu/ifpb/padroes/api/pizzahot/proxy/PizzaHotServiceProxy.java
+++ src/main/java/br/edu/ifpb/padroes/api/pizzahot/proxy/PizzaHotServiceProxy.java
@@ -1,14 +1,28 @@
 package br.edu.ifpb.padroes.api.pizzahot.proxy;
 
 import br.edu.ifpb.padroes.api.pizzahot.PizzaHotPizza;
+import br.edu.ifpb.padroes.api.pizzahot.PizzaHotServiceImpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // TODO - implementar proxy de cache para evitar leitura do json a cada chamada do método
 public class PizzaHotServiceProxy implements PizzaHotService {
+    private PizzaHotServiceImpl pizzaHotService;
+    private List<PizzaHotPizza> pizzasCache = new ArrayList<>();
+
+    public PizzaHotServiceProxy(PizzaHotServiceImpl pizzaHotService) {
+        this.pizzaHotService = pizzaHotService;
+    }
+
     @Override
     public List<PizzaHotPizza> getPizzas() {
+        System.out.println("Retornando o cache....");
         // TODO - implementar proxy
-        return null;
+        if (this.pizzasCache.isEmpty()) {
+            this.pizzasCache = this.pizzaHotService.getPizzas();
+        }
+
+        return this.pizzasCache;
     }
 }
